b8447b6d81708bcf55e3dc860dc054cbfe8e58e2
Aisha Yusuff <a.yusuff99@outlook.com>
2023-04-26 21:24:35 +0100

    Move input to homepage and create home template

--- application/app.py
+++ application/app.py
@@ -5,52 +5,61 @@ from sqlalchemy import true
 app = Flask(__name__)
 
 connection = pymysql.connect(host="localhost",
-                         port=3306,
-                         user="root",
-                         password="password",
-                         database="githuns",
-                         charset="utf8mb4",
-                         cursorclass=pymysql.cursors.DictCursor)
-
+                             port=3306,
+                             user="root",
+                             password="password",
+                             database="githuns",
+                             charset="utf8mb4",
+                             cursorclass=pymysql.cursors.DictCursor)
 
 categories = ["General Knowledge", "Music", "History", "Movies", "Science"]
 currentUser = ''
 
+
 # https://neil.tesaluna.com/posts/connecting-flask-to-aws-rds/
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return "<h1>Home Page</h1>"
-
-
-@app.route("/menu", methods=['GET', 'POST'])
-def menu():
     if request.method == 'POST' and 'name' in request.form:
         # fetch the form data - the user's name
         username = request.form['name']
 
-        connection.ping(reconnect=True)
 
         # save name to the db
         with connection:
             with connection.cursor() as cursor:
-                #         Insert form data into database
+            #    Insert form data into database
                 sql = "INSERT INTO scores(name) VALUES (%s)"
                 cursor.execute(sql, username)
-        #     commit connection to save changes to the database
+            #     commit connection to save changes to the database
             connection.commit()
-            print(f'username: {username} has been saved into db')
 
-    return render_template('menu.html', categories=categories)
+    print(currentUser)
+    return render_template('home.html')
+
+
+@app.route("/menu", methods=['GET', 'POST'])
+def menu():
+    connection.ping(reconnect=True)
+    #     select name from db
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT name FROM scores"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            currentUser = result
+    print(currentUser)
+    return render_template('menu.html', categories=categories, name=currentUser)
 
 
 @app.route("/start/<category>")
 def start(category):
-    #     select name from db
+    connection.ping(reconnect=True)
 
+    #     select name from db
     with connection:
         with connection.cursor() as cursor:
-            sql = "SELECT name FROM scores ORDER BY ID DESC LIMIT 1"
+            sql = "SELECT name FROM scores"
             cursor.execute(sql)
             result = cursor.fetchone()
             currentUser = result
@@ -58,10 +67,8 @@ def start(category):
     return render_template('start.html', category=category, name=currentUser)
 
 
-
 # run the app in debug mode
 # so app reloads when changes have been made
 if __name__ == "__main__":
     # dbconnect()
     app.run(debug=True)
-
